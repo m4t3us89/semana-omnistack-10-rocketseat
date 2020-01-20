02cad4d39aab3fd2e779c6caf7c08a1fe757d8e2
Allisson Mateus <allissonmateus89@gmail.com>
2020-01-19 21:17:09 -0300

    Init Mobile With Expo

--- web/src/Main.js
+++ web/src/Main.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import './SideBar.css'
 import './Main.css'
+import api from './services/api'
 
-function Main (props) {
-  const [geoLocation, setGeolocation] = useState()
+
+function Main () {
+  const [github_username, setGithubUsername] = useState('m4t3us89')
+  const [techs, setTechs] = useState('Reactjs')
+  const [longitude, setLongitude] = useState('')
+  const [latitude, setLatitude] = useState('')
+  const [devs, setDevs] = useState([])
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       position => {
-        console.log(position)
+        const { latitude, longitude } = position.coords
+        setLongitude(longitude)
+        setLatitude(latitude)
       },
       err => {
         console.log(err)
@@ -19,31 +27,59 @@ function Main (props) {
     )
   }, [])
 
+  useEffect(()=>{
+      async function loadDevs(){
+        const response = await api.get('/devs')
+        setDevs(response.data)
+      }
+
+      loadDevs()
+  }, [])
+
+  async function handleAddDev(e){
+    e.preventDefault()
+
+    try{
+      const response = await api.post('/devs',{
+        github_username,
+        longitude,
+        latitude,
+        techs
+      })
+
+      setGithubUsername('')
+      setTechs('')
+      setDevs([...devs,response.data])
+    }catch(err){
+      console.log('error ' + err.data)
+    }
+
+  }
+
   return (
     <>
       <aside>
         <strong>Cadastrar</strong>
-        {geoLocation && geoLocation.teste}
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className='input-block'>
             <label htmlFor='github_username'>Usuário do Github</label>
-            <input name='github_username' id='github_username' required />
+            <input name='github_username' id='github_username' required value={github_username} onChange={e=>setGithubUsername(e.target.value)}/>
           </div>
 
           <div className='input-block'>
             <label htmlFor='techs'>Tecnologias</label>
-            <input name='techs' id='techs' required />
+            <input name='techs' id='techs' required value={techs} onChange={e=>setTechs(e.target.value)}/>
           </div>
 
           <div className='input-group'>
             <div className='input-block'>
               <label htmlFor='latitude'>Latitude</label>
-              <input name='latitude' id='latitude' required />
+              <input type='number' name='latitude'   id='latitude' required value={latitude} onChange={e=>setLatitude(e.target.value)}/>
             </div>
 
             <div className='input-block'>
               <label htmlFor='longitude'>Longitude</label>
-              <input name='longitude' id='longitude' required />
+              <input type='number' name='longitude' id='longitude'  required value={longitude} onChange={e=>setLongitude(e.target.value)}/>
             </div>
           </div>
 
@@ -53,86 +89,24 @@ function Main (props) {
 
       <main>
         <ul>
-          <li className='dev-item'>
-            <header>
-              <img
-                src='https://avatars0.githubusercontent.com/u/32775082?v=4'
-                alt='Allisson Mateus'
-              />
-              <div className='user-info'>
-                <strong>Allisson Mateus</strong>
-                <span>Reactjs, React Native, Nodejs</span>
-              </div>
-            </header>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English.
-            </p>
-            <a href='https://github.com/m4t3us89'>Acessar perfil no Github</a>
-          </li>
-          <li className='dev-item'>
-            <header>
-              <img
-                src='https://avatars0.githubusercontent.com/u/32775082?v=4'
-                alt='Allisson Mateus'
-              />
-              <div className='user-info'>
-                <strong>Allisson Mateus</strong>
-                <span>Reactjs, React Native, Nodejs</span>
-              </div>
-            </header>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English.
-            </p>
-            <a href='https://github.com/m4t3us89'>Acessar perfil no Github</a>
-          </li>
-          <li className='dev-item'>
-            <header>
-              <img
-                src='https://avatars0.githubusercontent.com/u/32775082?v=4'
-                alt='Allisson Mateus'
-              />
-              <div className='user-info'>
-                <strong>Allisson Mateus</strong>
-                <span>Reactjs, React Native, Nodejs</span>
-              </div>
-            </header>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English.
-            </p>
-            <a href='https://github.com/m4t3us89'>Acessar perfil no Github</a>
-          </li>
-          <li className='dev-item'>
-            <header>
-              <img
-                src='https://avatars0.githubusercontent.com/u/32775082?v=4'
-                alt='Allisson Mateus'
-              />
-              <div className='user-info'>
-                <strong>Allisson Mateus</strong>
-                <span>Reactjs, React Native, Nodejs</span>
-              </div>
-            </header>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English.
-            </p>
-            <a href='https://github.com/m4t3us89'>Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev=>(
+              <li key={dev._id} className='dev-item'>
+              <header>
+                <img
+                  src={dev.avatar_url}
+                  alt={dev.name}
+                />
+                <div className='user-info'>
+                <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>
+               {dev.bio}
+              </p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+            </li>
+          ))}
         </ul>
       </main>
     </>
