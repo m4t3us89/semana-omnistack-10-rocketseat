1ee47562981dfed8f86988039808a58feacdf5d8
Allisson Mateus Melo Reis <allisson.reis@tjto.jus.br>
2020-01-27 11:17:02 -0300

    Styles Maps

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,45 +1,54 @@
-import React, { useEffect , useState} from 'react';
-import { StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, Dimensions } from 'react-native'
 import MapView from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
 // import { Container } from './styles';
 
-export default function Main() {
+export default function Main () {
   const [currentRegion, setCurrentRegion] = useState(null)
 
-  useEffect(()=>{
-      async function loadInitialPosition(){
-        const { granted } = await requestPermissionsAsync()
+  useEffect(() => {
+    async function loadInitialPosition () {
+      const { granted } = await requestPermissionsAsync()
 
-        if(granted){
-            const { coords } = await getCurrentPositionAsync({
-                enableHighAccuracy: true
-            })
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        })
 
-            const { latitude, longitude } = coords
-            setCurrentRegion({
-                latitude,
-                longitude,
-                latitudeDelta: 0.04,
-                longitudeDelta: 0.04,
-            })
-        }
+        const { latitude, longitude } = coords
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        })
       }
-      loadInitialPosition()
+    }
+    loadInitialPosition()
   })
 
-  if (!currentRegion){
-      return null
+  if (!currentRegion) {
+    return null
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}/>
-  );
+    <View style={styles.container}>
+      <MapView initialRegion={currentRegion} style={styles.mapStyle} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    map: {
-        flex: 1
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
 })
